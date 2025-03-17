ef2af6a57d7d0ce8c0aa1a773b0193a5b84d61d3
Aryagorjipour <arygorji.pour@gmail.com>
2025-03-17 21:36:59 +0330

    fix: move config file next to the executable file.

--- install_windows.bat
+++ install_windows.bat
@@ -15,24 +15,17 @@ if "%LATEST_RELEASE%"=="" (
 echo Downloading latest version from: %LATEST_RELEASE%
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%LATEST_RELEASE%', 'spwd.exe')"
 
-:: Move the file to a location in the PATH
-echo Moving executable to C:\Windows\System32
-move spwd.exe C:\Windows\System32 >nul 2>&1
+:: Move the file to a location in the PATH (System32)
+set INSTALL_DIR=C:\Windows\System32
+echo Moving executable to %INSTALL_DIR%
+move spwd.exe %INSTALL_DIR% >nul 2>&1
 
-:: Ensure C:\ProgramData\spwd\ exists
-if not exist "C:\ProgramData\spwd\" mkdir "C:\ProgramData\spwd"
-
-:: Ensure the database exists
-if not exist "C:\ProgramData\spwd\passwords.db" (
-    echo Creating database...
-    type nul > "C:\ProgramData\spwd\passwords.db"
-)
-
-:: Download config.sample.json
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aryagorjipour/spwd/main/config.sample.json', 'C:\ProgramData\spwd\config.sample.json')"
+:: Download config.sample.json next to spwd.exe
+echo Downloading config.sample.json...
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aryagorjipour/spwd/main/config.sample.json', '%INSTALL_DIR%\config.sample.json')"
 
 :: Verify if the file was downloaded
-if not exist "C:\ProgramData\spwd\config.sample.json" (
+if not exist "%INSTALL_DIR%\config.sample.json" (
     echo Error: Failed to download config.sample.json
     exit /b 1
 )
@@ -41,7 +34,7 @@ if not exist "C:\ProgramData\spwd\config.sample.json" (
 for /f %%A in ('powershell -Command "[Convert]::ToBase64String((1..32 | % {Get-Random -Maximum 256}))"') do set SECRET_KEY=%%A
 
 :: Create config.json by replacing the placeholder in config.sample.json
-powershell -Command "(Get-Content 'C:\ProgramData\spwd\config.sample.json') -replace '\"secret_key\": *\".*\"', '\"secret_key\": \"%SECRET_KEY%\"' | Set-Content 'C:\ProgramData\spwd\config.json'"
+powershell -Command "(Get-Content '%INSTALL_DIR%\config.sample.json') -replace '\"secret_key\": *\".*\"', '\"secret_key\": \"%SECRET_KEY%\"' | Set-Content '%INSTALL_DIR%\config.json'"
 
 echo Installation completed successfully!
 echo You can now run 'spwd' from any command prompt.
